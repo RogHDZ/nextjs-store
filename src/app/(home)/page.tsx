b29323a7cac13fd63345b1afdac805eba67a3379
Rogelio <royhs23@gmail.com>
2024-03-22 13:36:39 -0600

    route grouping

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { MainProducts } from "../components/home/MainProducts";
+export default function Home() {
+  return (
+    <main>
+      <MainProducts />
+    </main>
+  );
+}
